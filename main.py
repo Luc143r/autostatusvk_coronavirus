ed29522a4ba36ac796d325f01ad181bd937a6d61
StupidDeveloper13 <48052307+StupidDeveloper13@users.noreply.github.com>
2020-04-20 17:44:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from coronapenza import parsepenza
 from time import sleep
 
 token = '3bbfeb7eb6b6ac946ee804565628cfba31ca60e341e0dd6018f7afdd6a36d844eb72d9acf7e4dbab73836'
-kd = 120
+kd = 180
 
 session = vk.Session(access_token=token)
 api = vk.API(session, v = "5.95")
+print('Autostatus start')
 
 while True:
+    print('Autostatus update')
     status = parsepenza()
     api.status.set(text = status)
     sleep(kd)

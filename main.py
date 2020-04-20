ba5aac8af79b9900e91526abbc7d46f6cce41af4
StupidDeveloper13 <48052307+StupidDeveloper13@users.noreply.github.com>
2020-04-20 22:09:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from coronapenza import parsepenza
 from time import sleep
 
 token = '3bbfeb7eb6b6ac946ee804565628cfba31ca60e341e0dd6018f7afdd6a36d844eb72d9acf7e4dbab73836'
-kd = 180
+kd = 300
 
 session = vk.Session(access_token=token)
 api = vk.API(session, v = "5.95")
